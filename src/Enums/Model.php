040d37cd0735a3662885582af8cc2c5dc77bc3e6
ceyhun-celik <ceyhuniverse@gmail.com>
2024-10-23 11:27:32 +0300

    refactor: available models updated

--- src/Enums/Model.php
+++ src/Enums/Model.php
@@ -4,24 +4,39 @@ namespace CeytekLabs\OpenAI\Enums;
 
 enum Model: string
 {
+    case GPT_4_TURBO = 'gpt-4-turbo';
+    case GPT_4_TURBO_2024_04_09 = 'gpt-4-turbo-2024-04-09';
     case TTS_1 = 'tts-1';
     case TTS_1_1106 = 'tts-1-1106';
+    case CHATGPT_4O_LATEST = 'chatgpt-4o-latest';
     case DALL_E_2 = 'dall-e-2';
+    case WHISPER_1 = 'whisper-1';
+    case GPT_4O_2024_08_06 = 'gpt-4o-2024-08-06';
+    case GPT_4_TURBO_PREVIEW = 'gpt-4-turbo-preview';
     case GPT_3_5_TURBO_INSTRUCT = 'gpt-3.5-turbo-instruct';
-    case GPT_3_5_TURBO_0125 = 'gpt-3.5-turbo-0125';
+    case GPT_4O = 'gpt-4o';
+    case GPT_4_0125_PREVIEW = 'gpt-4-0125-preview';
     case GPT_3_5_TURBO = 'gpt-3.5-turbo';
+    case GPT_3_5_TURBO_0125 = 'gpt-3.5-turbo-0125';
     case BABBAGE_002 = 'babbage-002';
     case DAVINCI_002 = 'davinci-002';
+    case GPT_4O_REALTIME_PREVIEW_2024_10_01 = 'gpt-4o-realtime-preview-2024-10-01';
     case DALL_E_3 = 'dall-e-3';
+    case GPT_4O_REALTIME_PREVIEW = 'gpt-4o-realtime-preview';
+    case GPT_4O_MINI = 'gpt-4o-mini';
+    case GPT_4O_2024_05_13 = 'gpt-4o-2024-05-13';
+    case GPT_4O_MINI_2024_07_18 = 'gpt-4o-mini-2024-07-18';
+    case GPT_4O_AUDIO_PREVIEW_2024_10_01 = 'gpt-4o-audio-preview-2024-10-01';
+    case GPT_4O_AUDIO_PREVIEW = 'gpt-4o-audio-preview';
     case TTS_1_HD = 'tts-1-hd';
     case TTS_1_HD_1106 = 'tts-1-hd-1106';
+    case GPT_4_1106_PREVIEW = 'gpt-4-1106-preview';
     case TEXT_EMBEDDING_ADA_002 = 'text-embedding-ada-002';
     case GPT_3_5_TURBO_16K = 'gpt-3.5-turbo-16k';
     case TEXT_EMBEDDING_3_SMALL = 'text-embedding-3-small';
     case TEXT_EMBEDDING_3_LARGE = 'text-embedding-3-large';
-    case WHISPER_1 = 'whisper-1';
     case GPT_3_5_TURBO_1106 = 'gpt-3.5-turbo-1106';
+    case GPT_4_0613 = 'gpt-4-0613';
+    case GPT_4 = 'gpt-4';
     case GPT_3_5_TURBO_INSTRUCT_0914 = 'gpt-3.5-turbo-instruct-0914';
-    case GPT_4O_MINI = 'gpt-4o-mini';
-    case GPT_4O_MINI_2024_07_18 = 'gpt-4o-mini-2024-07-18';
 }
